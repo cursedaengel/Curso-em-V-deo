ffbc2a441d70e9337354cfef36eee6e73654f546
Kelly <161160040+cursedaengel@users.noreply.github.com>
2024-12-10 02:12:52 -0300

    Create AT7_Media.py

---
+++ AT7_Media.py
@@ -0,0 +1,7 @@
+n1 = int(input("Digite o valor da primeira nota: "))
+n2 = int(input("Digite o valor da segunda nota: "))
+n3 = int(input("Digite o valor da terceira nota: "))
+
+media = (n1+n2+n3) / 3
+
+print ("A média do aluno foi: {}".format(media))

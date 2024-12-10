530ca36ba34749bcaab5d605ae07bb9df5a0a92d
Kelly <161160040+cursedaengel@users.noreply.github.com>
2024-12-10 02:11:35 -0300

    Create ATV5_AntecessorSucessor.py

---
+++ ATV5_AntecessorSucessor.py
@@ -0,0 +1,7 @@
+valor = int(input("Digite o seu valor: "))
+antecessor = (valor - 1)
+sucessor = (valor + 1)
+print ("Considerando o valor {}, seu antecessor é {} e o seu sucessor é {}".format (valor, antecessor, sucessor))
+
+valor = int(input("Digite o seu valor: "))
+print ("Considerando o valor {}, seu antecessor é {} e o seu sucessor é {}".format (valor, (valor - 1), (valor + 1)))

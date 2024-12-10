55022fe92fbf19384ed2177917fd665172cf5145
Kelly <161160040+cursedaengel@users.noreply.github.com>
2024-12-10 02:26:10 -0300

    Create ATV16_ParteInteira.py

---
+++ ATV16_ParteInteira.py
@@ -0,0 +1,7 @@
+import math
+num = float(input("Digite um valor: "))
+print ("O inteiro de {} é {}".format(num, math.trunc(num)))
+
+from math import trunc
+num = float(input("Digite um valor: "))
+print ("O inteiro de {} é {}".format(num, int(num)))

2ed88d306ccd7d92bd55c7181fce57f79bb9c382
Kelly <161160040+cursedaengel@users.noreply.github.com>
2024-12-10 02:23:41 -0300

    Create ATV12_Desconto.py

---
+++ ATV12_Desconto.py
@@ -0,0 +1,3 @@
+preco = float(input("Qual é o preço do produto? "))
+novopreço = preco - (preco*5 /100)
+print ("O produto que custava {} passa a custar {} com 5% de desconto".format (preco, novopreço))

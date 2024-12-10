143fe3d7a137f0697fad2006822ba1435b60e08b
Kelly <161160040+cursedaengel@users.noreply.github.com>
2024-12-10 02:22:57 -0300

    Create ATV11_TintaParaParede.py

---
+++ ATV11_TintaParaParede.py
@@ -0,0 +1,5 @@
+altura = float(input("Digite a sua altura: "))
+largura = float(input("Digite a sua largura: "))
+area = largura * altura
+tinta = area / 2
+print ("Se a sua parede tem {} de altura e {} de largura, a área total é {} e precisa de {}L de tinta".format(altura, largura, area, tinta))

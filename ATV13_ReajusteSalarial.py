cda3d0d80fe5c64e9414f8032b981eefe50452c6
Kelly <161160040+cursedaengel@users.noreply.github.com>
2024-12-10 02:24:24 -0300

    Create ATV13_ReajusteSalarial.py

---
+++ ATV13_ReajusteSalarial.py
@@ -0,0 +1,4 @@
+# aumento de 15% no reajuste
+salario = float(input("Digite o valor do salário: "))
+novosalario = salario + (salario*15/100)
+print("O salário que antes era {:.2f} passa a ser {:.2f} com o novo ajuste.".format (salario, novosalario))

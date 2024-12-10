b919c66f479c1bf902b2430d197f7282ea5234e3
Kelly <161160040+cursedaengel@users.noreply.github.com>
2024-12-10 02:12:21 -0300

    Create ATV6_DobroTriploRaiz.py

---
+++ ATV6_DobroTriploRaiz.py
@@ -0,0 +1,7 @@
+n = int(input("Digite o valor do número: "))
+dobro = (n*2)
+triplo = (n*3)
+raiz = n ** (1/2)
+print ("O dobro do número {} é {}".format(n, dobro))
+print ("O triplo do número {} é {}".format(n, triplo))
+print ("A raiz do número {} é {}".format(n, raiz))
